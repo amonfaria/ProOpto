e20181a2b8cc917cbe0ec0e3f3a981429eb5dcd7
amongoiano <amon.faria@gmail.com>
2014-08-23 05:50:06 -0400

    Changed files exist - Auto updating

--- logout.php
+++ logout.php
@@ -15,8 +15,6 @@ include_once('main-menu.php');
 <div id='fg_membersite'class="splash-container-profile">
 <div class="splash">
 
-test
-
 <h2>Logout com sucesso</h2>
 <p>
 <a href='signin.php'>Login Novamente</a>
