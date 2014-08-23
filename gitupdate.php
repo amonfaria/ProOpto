9f28dfc1298c6620474c9af4489ab34537a496f9
amongoiano <amon.faria@gmail.com>
2014-08-23 03:46:42 -0400

    Changed files exist - Auto updating

--- gitupdate.php
+++ gitupdate.php
@@ -2,8 +2,6 @@
  
 // Use in the "Post-Receive URLs" section of your GitHub repo.
  
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /usr/share/portal/login/proopto/ && git reset --hard HEAD && git pull' );
-}
+echo shell_exec("/full/path/to/bin/git pull 2>&1");
  
 ?>
