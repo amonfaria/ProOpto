13f83f1c4482fb183411b9127813fbe922c88688
amongoiano <amon.faria@gmail.com>
2014-08-23 15:52:53 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -23,13 +23,13 @@
 </head>
 
 <body>
+<div class="row">
+  <div class="clearfix visible-xs-block">
+  <div class="col-xs-6 col-sm-4">hidden?</div>
+</div>
 
       <ul class="nav navbar-nav navbar-right">
-      <div class="row">
-        <div class="clearfix visible-xs-block">
-        <div class="col-xs-6 col-sm-4">hidden?</div>
-      </div>
-      </div>
+            </div>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
