a6e47c11d46bf51b5abc9d0b728d4fc58308a4b5
amongoiano <amon.faria@gmail.com>
2014-08-23 13:43:58 -0400

    Changed files exist - Auto updating

--- profile-home.php
+++ profile-home.php
@@ -42,14 +42,14 @@ GetMenu($fgmembersite->UserClass())
 <div class="portalMenu">
     <div class="colLeft">
     <div class="pure-menu pure-menu-open">
-        <a class="pure-menu-heading">Yahoo! Sites</a>
+        <a class="pure-menu-heading">Acesso</a>
     
         <ul>
             <li><a href="#">Flickr</a></li>
             <li><a href="#">Messenger</a></li>
             <li><a href="#">Sports</a></li>
             <li><a href="#">Finance</a></li>
-            <li class="pure-menu-heading">More Sites!</li>
+            <li class="pure-menu-heading">Administrar</li>
             <li><a href="#">Games</a></li>
             <li><a href="#">News</a></li>
             <li><a href="#">OMG!</a></li>
