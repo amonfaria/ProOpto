f89dff377933ed86fa2d7df22db645d0c117200c
amongoiano <amon.faria@gmail.com>
2014-08-23 15:06:41 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -2,53 +2,15 @@
 <div class="header">
     <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
         <a class="pure-menu-heading" href="">Pro Opto</a>
-<div class="yui3-skin-sam">
-<div id="demo-horizontal-menu">
-    <ul id="std-menu-items">
-        <li class="pure-menu-selected"><a href="#">Flickr</a></li>
-        <li><a href="#">Messenger</a></li>
-        <li><a href="#">Sports</a></li>
-        <li><a href="#">Finance</a></li>
-        <li>
-            <a href="#">Other</a>
-            <ul>
-                <li class="pure-menu-heading">More from Yahoo!</li>
-                <li class="pure-menu-separator"></li>
-                <li><a href="#">Autos</a></li>
-                <li><a href="#">Flickr</a></li>
-                <li><a href="#">Answers</a></li>
-                <li>
-                    <a href="#">Even More</a>
-                    <ul>
-                        <li><a href="#">Horoscopes</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">OMG</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
+<ul class="nav navbar-nav navbar-right">
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
+    <ul class="dropdown-menu" role="menu">
+      <li><a href="#">Action</a></li>
+      <li><a href="#">Another action</a></li>
+      <li><a href="#">Something else here</a></li>
+      <li class="divider"></li>
+      <li><a href="#">Logout</a></li>
     </ul>
-</div>
-</div>
-    </div>
-</div>
-
-<script>
-YUI({
-    classNamePrefix: 'pure'
-}).use('gallery-sm-menu', function (Y) {
-
-    var horizontalMenu = new Y.Menu({
-        container         : '#demo-horizontal-menu',
-        sourceNode        : '#std-menu-items',
-        orientation       : 'horizontal',
-        hideOnOutsideClick: false,
-        hideOnClick       : false
-    });
-
-    horizontalMenu.render();
-    horizontalMenu.show();
-
-});
-</script>
+  </li>
+</ul>
