34e6d6c2859786be8f1ea8778dfbfaf30c800f8e
amongoiano <amon.faria@gmail.com>
2014-08-23 15:45:00 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -44,11 +44,11 @@
   <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
 </div>
 <div class="row">
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+  <div class="col-xs-6 col-sm-4">ProOpto</div>
+  <div class="col-xs-6 col-sm-4">Search</div>
   <!-- Optional: clear the XS cols if their content doesn't match in height -->
   <div class="clearfix visible-xs-block"></div>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
+  <div class="col-xs-6 col-sm-4">logout</div>
 </div>
 
 </body>
