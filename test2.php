1e74da62d7ca2960720516cddd6ba248dcb74813
amongoiano <amon.faria@gmail.com>
2014-08-23 17:39:49 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -25,68 +25,23 @@
 <body>
 
 
-
-      <ul class="nav navbar-nav navbar-right">
-      <div class="row">
-         <div class="container-fluid">
-         <div class="clearfix hidden-xs">
-         
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
-          
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Logout</a></li>
-          </ul>
-          </div>
-          <div class="clearfix visible-xs-block">
-           
-          <li class="dropdown is-center">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
-            
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Logout</a></li>
-            </ul>
-            </div>
-        </li>
-        </div>
-        </div>
-		</div>
-      </ul>
-
-
-
-<div class="row">
-  <div class="col-xs-12 col-sm-6 col-md-8">.col-xs-12 .col-sm-6 .col-md-8</div>
-  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+ <body data-spy="scroll" data-target=".navbar-collapse">
+ <div class="navbar navbar-fixed-top" role="navigation">
+  <div class="container">
+   <div class="navbar-header">
+     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+     <span class="sr-only">Toggle navigation</span>
+     </button>
+     <a class="navbar-brand" href="index.html"> <img src="#"></a>
+   </div>
+ <div class="navbar-collapse collapse">
+   <ul class="nav navbar-nav navbar-right">
+    <li class="active"><a href="#home">Home</a></li>
+    <li><a href="#one">One</a></li>
+    <li><a href="#two">Two</a></li>
+    <li><a href="#three">Three</a></li>
+    <li><a href="#four">Four</a></li>
+   </ul>
+  </div>
+ </div>
 </div>
-<div class="row">
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-  <div class="col-xs-6 col-sm-4">.col-xs-6 .col-sm-4</div>
-  <!-- Optional: clear the XS cols if their content doesn't match in height -->
-  <div class="clearfix visible-xs-block">
-  <div class="col-xs-6 col-sm-4">hidden?</div>
-</div>
-</div>
-
-</body>
-
-<script>
-
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-$('#cssmenu #menu-button').on('click', function(){
-  var menu = $(this).next('ul');
-  if (menu.hasClass('open')) {
-    menu.removeClass('open');
-  } else {
-    menu.addClass('open');
-  }
-});
-</script>
