66cf101e093cd5658b4a7da3cd434d4ebc4c9b8e
amongoiano <amon.faria@gmail.com>
2014-08-23 13:40:37 -0400

    Changed files exist - Auto updating

--- profile-home.php
+++ profile-home.php
@@ -40,7 +40,23 @@ GetMenu($fgmembersite->UserClass())
 ?>
 <!-- Form Code Start -->
 <div class="portalMenu">
-    <div class="colLeft">one</div>
+    <div class="colLeft">
+    <div class="pure-menu pure-menu-open">
+        <a class="pure-menu-heading">Yahoo! Sites</a>
+    
+        <ul>
+            <li><a href="#">Flickr</a></li>
+            <li><a href="#">Messenger</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="#">Finance</a></li>
+            <li class="pure-menu-heading">More Sites!</li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">OMG!</a></li>
+        </ul>
+    </div>
+    
+    </div>
     <div class="colRight">two</div>
 </div>
 <!-- client-side Form Validations:
