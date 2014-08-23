35de8f9555c398817753e9fac31d6df59da50ac6
amongoiano <amon.faria@gmail.com>
2014-08-23 13:25:03 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<head>
-<title>WebCam Access</title>
-</head>
-<body>
-
-<video autoplay></video>
-<script src=”webcam.js”></script>
-<canvas id=’canvas’ width=’300′
-height=’300′></canvas>
-<script language=’JavaScript’>
-document.getElementById(‘capture’).onclick =
-function() {
-var video = document.
-querySelector(‘video’);
-var canvas = document.
-getElementById(‘canvas’);
-var ctx = canvas.getContext(’2d’);
-ctx.drawImage(video,0,0);
-}
-</script>
-
-
-<div id=”webcam”></div>
-<p><a href=”JavaScript:webcam.
-capture();void(0);”>Smile!</a></p>
-<script type=”text/JavaScript”>
-jQuery(“#webcam”).webcam({
-width: 320,
-height: 240,
-mode: “callback”,
-swffile: “jscam.swf”,
-});
-</script>
-
-
-</body>
-</html>
+<div class="portalMenu">
+    <div class="colLeft">one</div>
+    <div class="colRight">two</div>
+</div>
