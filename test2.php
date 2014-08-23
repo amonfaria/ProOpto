595df17a2ad2d179342c47fe9ff04710525e3eb6
amongoiano <amon.faria@gmail.com>
2014-08-23 14:40:51 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -9,40 +9,42 @@
 <script src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
 </head>
 
-<body class="yui3-skin-sam">
+<body>
 
-<div id="productsandservices" class="yui3-menu yui3-menu-horizontal"><!-- Bounding box -->
-    <div class="yui3-menu-content"><!-- Content box -->
-        <a href="#">Other</a>
-        <ul>
-
-           <li><a href="#">Horoscopes</a></li>
-           <li><a href="#">Games</a></li>
-           <li><a href="#">Jobs</a></li>
-           <li><a href="#">OMG</a></li>
-           
-            <!-- Menu items -->
-        </ul>
-    </div>
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='index.html'><span>Home</span></a></li>
+   <li class='has-sub'><a href='#'><span>Products</span></a>
+      <ul>
+         <li class='has-sub'><a href='#'><span>Product 1</span></a>
+            <ul>
+               <li><a href='#'><span>Sub Item</span></a></li>
+               <li class='last'><a href='#'><span>Sub Item</span></a></li>
+            </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Product 2</span></a>
+            <ul>
+               <li><a href='#'><span>Sub Item</span></a></li>
+               <li class='last'><a href='#'><span>Sub Item</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>About</span></a></li>
+   <li class='last'><a href='#'><span>Contact</span></a></li>
+</ul>
 </div>
-
 </body>
 
 <script>
 
-//  Call the "use" method, passing in "node-menunav".  This will load the
-//  script and CSS for the MenuNav Node Plugin and all of the required
-//  dependencies.
-
-YUI().use('node-menunav', function(Y) {
-
-    //  Retrieve the Node instance representing the root menu
-    //  (<div id="productsandservices">) and call the "plug" method
-    //  passing in a reference to the MenuNav Node Plugin.
-
-    var menu = Y.one("#productsandservices");
-
-    menu.plug(Y.Plugin.NodeMenuNav);
-
+$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+$('#cssmenu #menu-button').on('click', function(){
+  var menu = $(this).next('ul');
+  if (menu.hasClass('open')) {
+    menu.removeClass('open');
+  } else {
+    menu.addClass('open');
+  }
 });
 </script>
