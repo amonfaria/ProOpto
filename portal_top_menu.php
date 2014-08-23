e1f63d55182f2c0d52a764abfb516d98db16ff18
amongoiano <amon.faria@gmail.com>
2014-08-23 18:07:56 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -1,11 +1,4 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 <nav class="navbar navbar-static-top" role="navigation">
   <div class="container">
