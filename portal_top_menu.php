4b2f5e37ebd84e2cae81de00fce2b8bb7e79a4cb
amongoiano <amon.faria@gmail.com>
2014-08-23 17:04:55 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -10,13 +10,25 @@
 
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <a href="#" class="navbar-brand"> ProOpto</a>
+  <button class= "navbar-toggle" data-toggle="collapse" data-target="full-top-menu">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      
+  </button>
+  
+
+  
+  <div class="collapse navbar-collapse" id="full-top-menu">
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+      <div class="row">
+      <div class="clearfix hidden-xs">
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
@@ -29,6 +41,19 @@
           </ul>
         </li>
       </ul>
+      </div>
+      <div class="clearfix visible-xs-block">
+      
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href ="#">Action</a></li>
+          <li><a href ="#">Another action</a></li>
+          <li><a href ="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href ="#">Logout</a></li>
+          
+      </ul>
+      </div>
+      </div>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
