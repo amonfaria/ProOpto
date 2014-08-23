b33ff5a25a01a5fa8e2e6e1d636081dfd74a371a
amongoiano <amon.faria@gmail.com>
2014-08-23 13:30:34 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -1,4 +1,12 @@
+<head>
+<link href="/css/layouts/marketing.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+
 <div class="portalMenu">
     <div class="colLeft">one</div>
     <div class="colRight">two</div>
 </div>
+
+</body>
