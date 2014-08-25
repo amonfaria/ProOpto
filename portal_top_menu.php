d02c11d551ebfd89e4a80aac76e7a763bc31b5dc
amongoiano <amon.faria@gmail.com>
2014-08-24 21:52:14 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -15,7 +15,7 @@
   <div class="collapse navbar-collapse fullMenuTopCollapse">
 
       <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Settings</a></li>
+      <li><a class="menuOrangeBorder" href="#">Settings</a></li>
       <li><a href="#">Another action</a></li>
       <li><a href="#">Something else here</a></li>
         <li class="dropdown">
