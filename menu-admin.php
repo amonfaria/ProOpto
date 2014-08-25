30a95ccca4578d6765b37e56867a98e89dd81faa
amongoiano <amon.faria@gmail.com>
2014-08-25 00:19:21 -0400

    Changed files exist - Auto updating

--- menu-admin.php
+++ menu-admin.php
@@ -1,9 +1,7 @@
+<li><a href="#">Contas</a></li>
+<li><a href="#">Calendario</a></li>
+<li><a href="#">Clinica</a></li>
+<li><a href="#">Finance</a></li>
 
-            <li><a href="#">Flickr</a></li>
-            <li><a href="#">Messenger</a></li>
-            <li><a href="#">Sports</a></li>
-            <li><a href="#">Finance</a></li>
-            <li><a href="#">Games</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">OMG!</a></li>
+
  
