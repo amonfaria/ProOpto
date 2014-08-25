3a06c48f046ee6972ecfe88d911c55d9a2348d32
amongoiano <amon.faria@gmail.com>
2014-08-24 23:23:34 -0400

    Changed files exist - Auto updating

--- show-collapsed-menu.php
+++ show-collapsed-menu.php
@@ -1,9 +1,7 @@
 
 
-<div class="row visible-xs-block">
 <?php
 require_once("./include/membersite_config.php");
 require_once("./include/find_my_menu.php");
 GetMenu($fgmembersite->UserClass()) 
 ?>
-</div>
