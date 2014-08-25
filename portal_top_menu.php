91e052df1a95594630cfebb220955172ed7674e5
amongoiano <amon.faria@gmail.com>
2014-08-24 22:45:01 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -15,9 +15,8 @@
   <div class="collapse navbar-collapse fullMenuTopCollapse">
 
       <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Settings</a></li>
+      <li><a class="pure-menu pure-menu-open" href="#">Settings</a></li>
       <li><a href="#">blah1</a></li>
-      <li><a href="#">blah2</a></li>
       <li><a href="#">blah3</a></li>
       <li><a href="#">Settings</a></li>
 
