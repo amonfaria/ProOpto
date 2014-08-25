dcfc8af455b8d4724871015d66a071359afb4de4
amongoiano <amon.faria@gmail.com>
2014-08-25 01:59:14 -0400

    Changed files exist - Auto updating

--- menu-admin.php
+++ menu-admin.php
@@ -1,5 +1,5 @@
 
-<li><a name="view_access" href="profile-home.php?view_access=1">Acesso</a></li>
+<li><a id="view_access" href="profile-home.php">Acesso</a></li>
 <li><a href="#">Calendario</a></li>
 <li><a href="#">Clinica</a></li>
 <li><a href="#">Finance</a></li>
@@ -7,8 +7,9 @@
 
 <script language="javascript"> 
 
-   function DoPostViewAccess(){
-      $.post("profile-home.php", { name: "view_access", value: "view_access" } );  //Your values here..
+
+   $('a#view_access').click(function DoPostViewAccess(){
+      $.post("profile-home.php", { name: "view_access", value: "view_access" } ));  //Your values here..
    }
 
 </script>
