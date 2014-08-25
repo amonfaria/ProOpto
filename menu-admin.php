14ddcc7ba3ba907d56be2ed3588cb2d8b1ad4854
amongoiano <amon.faria@gmail.com>
2014-08-25 00:33:48 -0400

    Changed files exist - Auto updating

--- menu-admin.php
+++ menu-admin.php
@@ -1,5 +1,7 @@
+<a class="pure-menu-heading">Acesso</a>
 <li><a href="#">Contas</a></li>
 <li><a href="#">Calendario</a></li>
+<a class="pure-menu-heading">Admin</a>
 <li><a href="#">Clinica</a></li>
 <li><a href="#">Finance</a></li>
 
--- profile-home.php
+++ profile-home.php
@@ -43,9 +43,7 @@ include_once('portal_top_menu.php');
 <div class="portalMenu row hidden-xs">
     <div class="colLeft">
     <div class="pure-menu">
-        <a class="pure-menu-heading">Acesso</a>
-    
-        
+     
             <?php
             GetMenu($fgmembersite->UserClass())
             ?>
