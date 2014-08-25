70c16646008748e76137e3a0173362827483525f
amongoiano <amon.faria@gmail.com>
2014-08-25 03:01:43 -0400

    Changed files exist - Auto updating

--- menu-admin.php
+++ menu-admin.php
@@ -3,16 +3,18 @@
 <input type='hidden' name='view_access' id='view_access' value='view_access'/>
 <li><a id="view_access" href="javascript:DoPost()">Acesso</a></li>
 </form>
-<li><a href="javascript:DoPost()">Calendario</a></li>
-
+<form id="view_calendar" action="profile-home.php" method="post">
+<input type='hidden' name='view_calendar' id='view_calendar' value='view_calendar'/>
+<li><a href="javascript:DodiffPost()">Calendario</a></li>
+</form>
 <li><a href="#">Clinica</a></li>
 <li><a href="#">Finance</a></li>
 
 
 <script language="javascript"> 
-function DoPost(){
-     $.post("profile-home.php", { name: "view_calendar", value: "view_calendar" id= "view_caendar"} );  //Your values here..
-  }
+   function DoPost(){  
+      $("view_access").submit();  
+   }
 
 </script>
 
