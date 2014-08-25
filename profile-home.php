544f1a2e228e6f3b079637d6dfdca00ffded0332
amongoiano <amon.faria@gmail.com>
2014-08-25 01:14:06 -0400

    Changed files exist - Auto updating

--- profile-home.php
+++ profile-home.php
@@ -52,13 +52,7 @@ include_once('portal_top_menu.php');
     
     </div>
     <div class="colRight">two</div>
-    <?php
-    if(isset($_POST['view_access']))
-    {
-        ViewAccess($fgmembersite->UserClass())
-    
-    }
-    ?>
+
 </div>
 </ul>
 <!-- Form Code Start -->
