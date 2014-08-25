1a1fb84f1290879ffcea32b497e1d7b798cdb109
amongoiano <amon.faria@gmail.com>
2014-08-25 01:26:34 -0400

    Changed files exist - Auto updating

--- view_access.php
+++
@@ -1,7 +0,0 @@
-<?php 
-   $result=$fgmembersite->DisplayAccess();
-   while($row = mysqli_fetch_array($result)) {
-     echo $row['username'] . " " . $row['phone_number'];
-     echo "<br>";
-   }
-?>
