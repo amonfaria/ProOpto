fd596c3723f6f0bf7422933d801f28fefe9442f8
amongoiano <amon.faria@gmail.com>
2014-08-25 01:27:22 -0400

    Changed files exist - Auto updating

--- include/find_my_menu.php
+++ include/find_my_menu.php
@@ -10,17 +10,6 @@ function GetMenu($userClass)
 
    }
    
-function ViewAccess($userClass)
-    {
-        if ($userClass=="admin"){
-            include('view_access.php');
-        }
-        else{
-            include('not_authorized.php');
-    }
-    
-
-   }
 
 
 ?>
