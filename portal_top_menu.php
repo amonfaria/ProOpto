79098e945197a74853947f1d42e05c944ffea25c
amongoiano <amon.faria@gmail.com>
2014-08-24 22:00:12 -0400

    Changed files exist - Auto updating

--- portal_top_menu.php
+++ portal_top_menu.php
@@ -16,8 +16,11 @@
 
       <ul class="nav navbar-nav navbar-right">
       <li><a class="menuOrangeBorder" href="#">Settings</a></li>
-      <li><a href="#">Another action</a></li>
-      <li><a href="#">Something else here</a></li>
+      <li><a class="menuOrangeBorder" href="#">blah1</a></li>
+      <li><a class="menuOrangeBorder" href="#">blah2</a></li>
+      <li><a class="menuOrangeBorder" href="#">blah3</a></li>
+      <li><a class="menuOrangeBorder" href="#">Settings</a></li>
+
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
