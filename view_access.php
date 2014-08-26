33eb949c5ccbe0cfb88c27104d820e12cf733a6d
amongoiano <amon.faria@gmail.com>
2014-08-26 19:54:07 -0400

    Changed files exist - Auto updating

--- view_access.php
+++ view_access.php
@@ -1,3 +1,14 @@
+<div class="portalMenu ">
+    <div class="pure-menu">
+     <form id="view_access" action="profile-home.php" method="post">
+     <input type='hidden' name='view_access' id='view_access' value='view_access'/>
+     <li><a id="view_access" href="javascript:DoViewPost()">Adicionar</a></li>
+     </form>
+        
+    </div>
+    
+    </div>
+</div>
 <?php 
    
    
