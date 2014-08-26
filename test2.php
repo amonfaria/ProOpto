ddb9708d30d1fa34781a1c9c563eac095f78e595
amongoiano <amon.faria@gmail.com>
2014-08-26 18:58:16 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -1,23 +1,26 @@
 <?php
 $con=mysqli_connect("localhost","root","Agoiano1","login");
-
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+$result = mysqli_query($con,"SELECT * FROM member2");
 
-        $qry = "Select * from member2 where company='485758484'";
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
 
-        $result = mysqli_query($qry,$this->connection);
-        
-        if(!$result || mysqli_num_rows($result) <= 0)
-        {
-            echo" no rows";
-        }
-        
-        
-        return "pass";
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['email'] . "</td>";
+  echo "</tr>";
+}
+
+echo "</table>";
 
 mysqli_close($con);
 ?>
