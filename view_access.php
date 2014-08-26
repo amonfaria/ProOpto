f1b43ce96d5e0fc6bf00c6075ee4014c7c72c33f
amongoiano <amon.faria@gmail.com>
2014-08-26 19:12:44 -0400

    Changed files exist - Auto updating

--- view_access.php
+++ view_access.php
@@ -3,11 +3,7 @@
    
    $result=array();
    $result = $fgmembersite->DisplayAccess();
-   foreach ($result as $row)  {
-
-     echo "name" . $row['name'] . "h";
-     echo "email" . $row['email'] . "h";
-   }
+   printf($result);
    
 
    
