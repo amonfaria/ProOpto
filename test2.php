8f630590d700012b49f06741f93ecd54248997ea
amongoiano <amon.faria@gmail.com>
2014-08-25 23:30:01 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -1,53 +1,23 @@
 <?php
-include_once('main-header.php');
+$con=mysqli_connect("localhost","root","Agoiano1","login");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+        $qry = "Select * from member2 where company='485758484'";
+
+        $result = mysqli_query($qry,$this->connection);
+        
+        if(!$result || mysqli_num_rows($result) <= 0)
+        {
+            echo" no rows";
+        }
+        
+        
+        return "pass";
+
+mysqli_close($con);
 ?>
-
-
-<nav class="navbar navbar-static-top" role="navigation">
-  <div class="container">
-  <a href="#" class="navbar-brand"> ProOpto</a>
-  <button class= "navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      
-  </button>
-  
-
-  
-  <div class="collapse navbar-collapse fullMenuTopCollapse">
-
-      <ul class="nav navbar-nav navbar-right">
-      <ul>
-      <div class="visible-xs-block">
-<div class="Mobile">  
-        <li><a href="#">Flickr</a></li>
-        <li><a href="#">Messenger</a></li>
-        <li><a href="#">Sports</a></li>
-        <li><a href="#">Finance</a></li>
-        <li class="pure-menu-heading">Administrar</li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">OMG!</a></li>
-   
-</div>
-</div>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Logout</a></li>
-          </ul>
-        </li>
-        </ul>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
-
-
