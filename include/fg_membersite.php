dd995c6fe7325eebfa1ee6a613c0b7de87df34ce
amongoiano <amon.faria@gmail.com>
2014-08-25 23:18:03 -0400

    Changed files exist - Auto updating

--- include/fg_membersite.php
+++ include/fg_membersite.php
@@ -366,7 +366,7 @@ class FGMembersite
                     return false;
                 }
                 
-                $result="passed";
+                $result=1;
                 return $result;
     
     }
