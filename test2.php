fb81b46cf798de4114bf674992739483b32c4321
amongoiano <amon.faria@gmail.com>
2014-08-26 21:20:51 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -26,5 +26,9 @@
   </div>
 </div>
 
-
+<script language="javascript">
+$('#myModal').on('show.bs.modal', function (e) {
+  if (!data) return e.preventDefault() // stops modal from being shown
+})
+</script>
 </body>
