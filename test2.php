56ffc47eb0385148dc5bc624508e006d0e121b9e
amongoiano <amon.faria@gmail.com>
2014-08-26 21:12:12 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -21,3 +21,10 @@
     </div>
   </div>
 </div>
+
+
+<script language="javascript">
+$('#myModal').modal()                      // initialized with defaults
+$('#myModal').modal({ keyboard: false })   // initialized with no keyboard
+$('#myModal').modal('show')                // initializes and invokes show immediately
+</script>
