90a3823c58337960849652c122e75814e2d034a8
amongoiano <amon.faria@gmail.com>
2014-08-30 02:12:52 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -23,7 +23,12 @@
         
         $('#amon.faria@gmail.com').click(function() {
             $('#amon.faria@gmail.com').hide(500);
-        });  </script><div id='ifaria@gmail.com'><tr><td> isaque </td><td> isaque </td><td> ifaria@gmail.com </td><td><button id="ifaria@gmail.com" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
+        });  
+        </script>
+        
+        
+        
+  <div id='ifaria@gmail.com'><tr><td> isaque </td><td> isaque </td><td> ifaria@gmail.com </td><td><button id="ifaria@gmail.com" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
         
         $('#ifaria@gmail.com').click(function() {
             $('#ifaria@gmail.com').hide(500);
