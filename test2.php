9b9c7789e6b2b5d2a478fa5df41645facd1772e9
amongoiano <amon.faria@gmail.com>
2014-08-30 02:11:58 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -4,15 +4,30 @@
 ?>
 
 
-<div id='amonfaria'><tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button id="amonfaria" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
+<table class="pure-table pure-table-horizontal">
+    <thead>
+        <tr>
+            <label>Esperando Confirmação</label>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+
+<div id='amon.faria@gmail.com'><tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button id="amon.faria@gmail.com" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
         
-        $('#amonfaria').click(function() {
-            $('#amonfaria').hide(500);
+        $('#amon.faria@gmail.com').click(function() {
+            $('#amon.faria@gmail.com').hide(500);
+        });  </script><div id='ifaria@gmail.com'><tr><td> isaque </td><td> isaque </td><td> ifaria@gmail.com </td><td><button id="ifaria@gmail.com" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
+        
+        $('#ifaria@gmail.com').click(function() {
+            $('#ifaria@gmail.com').hide(500);
         });  </script>
-        
-        
- <div id='isaque faria'><tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button id="isaque faria" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
-         
-         $('#amon faria').click(function() {
-             $('#amon faria').hide(500);
-         });  </script>
+ 
+ </tbody>
+</table>
