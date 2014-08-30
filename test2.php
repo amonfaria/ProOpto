ced81422b0b1565eceda66e6577ca397d6e8e769
amongoiano <amon.faria@gmail.com>
2014-08-30 00:24:37 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -9,7 +9,7 @@
   <a href="#" data-toggle="tooltip" title="Some tooltip text!">Hover over me</a>
   
   <!-- Generated markup by the plugin -->
-  <div class="tooltip top" role="tooltip">
+  <div class="tooltip bottom" role="tooltip">
     <div class="tooltip-arrow"></div>
     <div class="tooltip-inner">
       Some tooltip text!
