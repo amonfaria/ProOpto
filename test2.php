ab654a00819baa6fa38a57a6ef7fca6a6ca2ffb6
amongoiano <amon.faria@gmail.com>
2014-08-30 00:23:47 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -5,40 +5,13 @@
 
 
 
-<nav class="navbar navbar-static-top" role="navigation">
-  <div class="container">
-  <a href="#" class="navbar-brand"> Zyne</a>
-  <button class= "navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      
-  </button>
+  <!-- HTML to write -->
+  <a href="#" data-toggle="tooltip" title="Some tooltip text!">Hover over me</a>
   
-
-  
-  <div class="collapse navbar-collapse fullMenuTopCollapse">
-
-      <ul class="nav navbar-nav navbar-right">
-      <div class="Mobile is-center pure-menu visible-xs-block">
-
-      </div>
-
-        <li class="dropdown">
-          <a href="#" class="Mobile dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="logout.php">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
-
-
+  <!-- Generated markup by the plugin -->
+  <div class="tooltip top" role="tooltip">
+    <div class="tooltip-arrow"></div>
+    <div class="tooltip-inner">
+      Some tooltip text!
+    </div>
+ </div>
