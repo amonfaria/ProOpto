864d8a840f6e27da4784cc5c02694b50a71e3a96
amongoiano <amon.faria@gmail.com>
2014-08-30 01:57:26 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -4,15 +4,25 @@
 ?>
 
 
-
-<a data-toggle="tooltip" title="add to">
-    <i class="icon-shopping-cart">Shopping</i>
-</a>
+<button class="hilang">Hilang!</button>
+<div id="muncul-1">
+<td><button id="muncul-1" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> hide me<i class="fa fa-times fa-lg"></i></button> </td>
+    <td>oi </td>
+    <td>dois </td>
+    <td>tres</td>
+    <td>quatro</td>
+</div>
+<button class="bukatutup">Muncul &amp; Hilang!</button>
+<div id="target"></div>
 
 <script>
-$('[data-toggle="tooltip"]').tooltip({
-    animated: 'fade',
-    placement: 'bottom',
+$('#muncul-1').click(function() {
+    $('#muncul-1').hide(500);
 });
-
+$('.hilang').click(function() {
+    $('#target').hide(500);
+});
+$('.bukatutup').click(function() {
+    $('#target').toggle('slow');
+});
 </script>
