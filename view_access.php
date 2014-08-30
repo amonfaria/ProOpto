c793da107b04e8ccadde8eccc3b19a1599fd83ea
amongoiano <amon.faria@gmail.com>
2014-08-30 01:32:36 -0400

    Changed files exist - Auto updating

--- view_access.php
+++ view_access.php
@@ -109,12 +109,14 @@ if (count($result))
     
     foreach ($result AS $id => $data) 
     { 
-        echo "<tr id='.$rejectstr.'>";
+        echo "<div id='.$rejectstr.'>";
+        echo "<tr>";
         echo "<td> $data[name] </td>";
         echo "<td> $data[name] </td>";
         echo "<td> $data[email] </td>";
-        echo '<td><button onclick="hide("'.$rejectstr.'")type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i>"</button> </td>';
+        echo '<td><button onclick="hide("'.$rejectstr.'")type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td>';
         echo '<td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td>';
+        echo "</div>";
         
         
         echo $acceptstr++;
