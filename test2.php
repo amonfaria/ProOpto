6c86a1808dd7b69927062fdff69fec8e7c9b449d
amongoiano <amon.faria@gmail.com>
2014-08-30 02:13:42 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -19,10 +19,10 @@
     <tbody>
 
 
-<div id='amon.faria@gmail.com'><tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button id="amon.faria@gmail.com" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
+<div id='amon'><tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button id="amon" type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr></div><script>  
         
-        $('#amon.faria@gmail.com').click(function() {
-            $('#amon.faria@gmail.com').hide(500);
+        $('#amon').click(function() {
+            $('#amon').hide(500);
         });  
         </script>
         
