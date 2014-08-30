b76c8593f0211f662767ecfc639011c6b1331787
amongoiano <amon.faria@gmail.com>
2014-08-30 00:42:36 -0400

    Changed files exist - Auto updating

--- view_access.php
+++ view_access.php
@@ -110,7 +110,7 @@ if (count($result))
         echo "<td> $data[name] </td>";
         echo "<td> $data[name] </td>";
         echo "<td> $data[email] </td>";
-        echo '<td><button type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button"> <i class="fa fa-times fa-lg"></i> </button> </td>';
+        echo '<td><button type="button" data-toggle="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i> </button> </td>';
         echo '<td><button type="button" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button"> <i class="fa fa-check fa-lg"></i> </button> </td>';
         
         
