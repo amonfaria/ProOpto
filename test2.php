91808e3f0c1354443950eb56c212abf595d31dca
amongoiano <amon.faria@gmail.com>
2014-08-30 03:43:30 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -319,6 +319,7 @@
     </div>
   </div>
 </div>
+<a href="#" data-confirm="Are you sure you want to delete?">Delete</a>
 
 <script>
 $('[data-toggle="tooltip"]').tooltip({
@@ -335,6 +336,20 @@ function hide(el_id){
     el_id.style.display="";
   }
 }
+
+
+$(document).ready(function() {
+	$('a[data-confirm]').click(function(ev) {
+		var href = $(this).attr('href');
+		if (!$('#dataConfirmModal').length) {
+			$('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+		} 
+		$('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
+		$('#dataConfirmOK').attr('href', href);
+		$('#dataConfirmModal').modal({show:true});
+		return false;
+	});
+});
 
 </script>
 
