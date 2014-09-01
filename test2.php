37cfca1172276e8a725554de6d777fa5f448450d
amongoiano <amon.faria@gmail.com>
2014-09-01 01:26:12 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -114,121 +114,6 @@
 
 
 
-
-
-<nav class="navbar navbar-static-top" role="navigation">
-  <div class="container">
-  <a href="#" class="navbar-brand"> Zyne</a>
-  <button class= "navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      
-  </button>
-  
-
-  
-  <div class="collapse navbar-collapse fullMenuTopCollapse">
-
-      <ul class="nav navbar-nav navbar-right">
-      <div class="Mobile is-center pure-menu visible-xs-block">
-      
-<form id="view_access" action="profile-home.php" method="post">
-<input type='hidden' name='view_access' id='view_access' value='view_access'/>
-<li><a id="view_access" href="javascript:DoViewPost()">Acesso</a></li>
-</form>
-<form id="view_calendar" action="profile-home.php" method="post">
-<input type='hidden' name='view_calendar' id='view_calendar' value='view_calendar'/>
-<li><a href="javascript:DoCalPost()">Calendario</a></li>
-</form>
-<form id="view_clinic" action="profile-home.php" method="post">
-<input type='hidden' name='view_calendar' id='view_calendar' value='view_calendar'/>
-<li><a href="javascript:DoClinicPost()">Clinica</a></li>
-</form>
-<li><a href="#">Finance</a></li>
-
-
-<script language="javascript"> 
-   function DoViewPost(){  
-      $("#view_access").submit();  
-   }
-   function DoCalPost(){  
-      $("#view_calendar").submit();  
-   }
-   function DoClinicPost(){  
-      $("#view_clinic").submit();  
-   }
-   function DoFinancePost(){  
-      $("#view_access").submit();  
-   }
-
-</script>
-
-
-       </div>
-
-        <li class="dropdown">
-          <a href="#" class="Mobile dropdown-toggle" data-toggle="dropdown">Nome <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="logout.php">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
-
-
-<ul>
-<div class="portalMenu row hidden-xs">
-    <div class="colLeft">
-    <div class="pure-menu">
-     
-            
-<form id="view_access" action="profile-home.php" method="post">
-<input type='hidden' name='view_access' id='view_access' value='view_access'/>
-<li><a id="view_access" href="javascript:DoViewPost()">Acesso</a></li>
-</form>
-<form id="view_calendar" action="profile-home.php" method="post">
-<input type='hidden' name='view_calendar' id='view_calendar' value='view_calendar'/>
-<li><a href="javascript:DoCalPost()">Calendario</a></li>
-</form>
-<form id="view_clinic" action="profile-home.php" method="post">
-<input type='hidden' name='view_calendar' id='view_calendar' value='view_calendar'/>
-<li><a href="javascript:DoClinicPost()">Clinica</a></li>
-</form>
-<li><a href="#">Finance</a></li>
-
-
-<script language="javascript"> 
-   function DoViewPost(){  
-      $("#view_access").submit();  
-   }
-   function DoCalPost(){  
-      $("#view_calendar").submit();  
-   }
-   function DoClinicPost(){  
-      $("#view_clinic").submit();  
-   }
-   function DoFinancePost(){  
-      $("#view_access").submit();  
-   }
-
-</script>
-
-
-         
-    </div>
-    
-    </div>
-    
-
 <div class="colRight">
 <div>
     <div>
@@ -289,90 +174,150 @@
     <tbody>
         
 
-<tr><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button type="button" data-toggle="tooltip" title="Editar" class="button-small-white pure-button"> <i class="fa fa-pencil fa-lg"></i> </button> </td><td><button type="button" data-toggle="tooltip" title="Deletar Usuario" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-trash fa-lg"></i> </button> </td></tr><tr><td> isaque </td><td> isaque </td><td> ifaria@gmail.com </td><td><button type="button" data-toggle="tooltip" title="Editar" class="button-small-white pure-button"> <i class="fa fa-pencil fa-lg"></i> </button> </td><td><button type="button" data-toggle="tooltip" title="Deletar Usuario" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-trash fa-lg"></i> </button> </td></tr> </tbody>
+<?php
+
+ $result = $fgmembersite->DisplayAccess();
+if (count($result)) 
+{ 
+    foreach ($result AS $id => $data) 
+    { 
+        echo "<tr>";
+        echo "<td class='medsize'> $data[name] </td>";
+        echo "<td class='medsize'> $data[name] </td>";
+        echo "<td class='medsize'> $data[email] </td>";
+        echo '<td class="medsize"><button type="button" rel="tooltip" title="Editar" class="button-small-white pure-button"> <i class="fa fa-pencil fa-lg"></i> </button> </td>';
+        echo '<td class="medsize"><button type="button" rel="tooltip" title="Deletar Usuario" data-confirm="Certeza que quer excluir?" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-trash fa-lg"></i> </button> </td>';
+        
+        
+        
+        echo "</tr>";
+    } 
+} 
+
+
+?>
+ </tbody>
 </table>
 
-<table class="pure-table pure-table-horizontal">
-    <thead>
-        <tr>
-            <label>Esperando Confirmação</label>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </tr>
-    </thead>
 
-    <tbody>
+<?php
+
+ $result = $fgmembersite->DisplayAccess();
+if (count($result)) 
+{ 
+    echo " <table class='pure-table pure-table-horizontal'>
+        <thead>
+            <tr>
+                <label>Esperando Confirmação</label>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+    
+        <tbody>";
+
+    
+    foreach ($result AS $id => $data) 
+    { 
+
+        echo "<tr id='$data[username]'>";
+        
+        echo "<td class='medsize'> $data[name] </td>";
+        echo "<td class='medsize'> $data[name] </td>";
+        echo "<td class='medsize'> $data[email] </td>";
+        echo '<td class="medsize"><button onclick="hide('."$data[username]".')" type="button" rel="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td>';
+        echo '<td class="medsize"><button onclick="hide('."$data[username]".')" type="button" rel="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td>';
+        
+        echo '';
+
+        echo "</tr>";
+        
+
+    } 
+} 
 
 
-<tr id="amon"><td> amon faria </td><td> amon faria </td><td> amon.faria@gmail.com </td><td><button type="button"  rel="tooltip" data-toggle="modal" data-target="verify-delete-modal" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" data-toggle="modal" data-toggle="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr><tr id='36'><td> isaque </td><td> isaque </td><td> ifaria@gmail.com </td><td><button onclick="hide(36)" type="button" rel="tooltip" title="Negar Acesso" class="button-small-red-inverse pure-button red-tooltip"> <i class="fa fa-times fa-lg"></i></button> </td><td><button type="button" rel="tooltip" title="Aprovar Acesso" class="button-small-green-inverse pure-button green-tooltip"> <i class="fa fa-check fa-lg"></i> </button> </td></tr>
+
+
+?>
+
  
  </tbody>
 </table>
 
-<div class="modal fade verify-delete-modal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content">
-      delete me?
-    </div>
-  </div>
-</div>
-<button href="#" data-toggle="tooltip" title="Aprovar Acesso" data-confirm="Certeza que quer excluir?">Delete</button>
 
-
-
-<div>
- <button href="#" rel="tooltip" title=" Novo usuario" class="button-small pure-button" data-toggle="modal" data-target="#basicModal">
- <i class="fa fa-plus-square fa-lg"></i>
- Novo
- 
- </button>
- 
- 
- <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-   <div class="modal-dialog">
-     <div class="modal-content">
-       <div class="modal-header">
-         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-         <h4 class="modal-title" id="myModalLabel">Registrar Usuario Novo</h4>
-       </div>
-       <div class="modal-body">
-         <form id="myform" method="get" action="something.php">
-             ADD REGISTRATION FIELDS HERE
-             <input type="text" name="name" />
-        
-         </form>
-       </div>
-       
-       <div class="modal-footer">
-         <button type="button" class="button-small-red pure-button" data-dismiss="modal">
-         <i class="fa fa-trash fa-lg"></i>
-         Cancelar
-         </button>
-         <button type="submit" form="myform" class="button-small pure-button">
-         <i class="fa fa-check-square fa-lg"></i>
-         Registrar
-         </button>
+    <div>
+     <button href="#" class="button-small pure-button" data-toggle="modal" data-target="#basicModal">
+     <i class="fa fa-plus-square fa-lg"></i>
+     Novo
+     
+     </button>
+     
+     
+     <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+       <div class="modal-dialog">
+         <div class="modal-content">
+           <div class="modal-header">
+             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+             <h4 class="modal-title" id="myModalLabel">Registrar Usuario Novo</h4>
+           </div>
+           <div class="modal-body">
+             <form id="myform" method="get" action="something.php">
+                 ADD REGISTRATION FIELDS HERE
+                 <input type="text" name="name" />
+            
+             </form>
+           </div>
+           
+           <div class="modal-footer">
+             <button type="button" class="button-small-red pure-button" data-dismiss="modal">
+             <i class="fa fa-trash fa-lg"></i>
+             Cancelar
+             </button>
+             <button type="submit" form="myform" class="button-small pure-button">
+             <i class="fa fa-check-square fa-lg"></i>
+             Registrar
+             </button>
+           </div>
+         </div>
        </div>
      </div>
-   </div>
- </div>
+        
+    </div>
     
+    </div>
 </div>
 
-</div>
 
+
+<form action ="profile-home.php" method="POST">
+<button class='btn btn-danger btn-xs' type="submit" name="remove_levels" value="delete"><span class="fa fa-times"></span> delete</button>
+</form>
+
+<div id="confirm" class="modal hide fade">
+  <div class="modal-body">
+    Are you sure?
+  </div>
+  <div class="modal-footer">
+    <button type="button" data-dismiss="modal" class="btn btn-primary" id="delete">Delete</button>
+    <button type="button" data-dismiss="modal" class="btn">Cancel</button>
+  </div>
+</div>
 
 
 <script>
-$('[data-toggle="tooltip"]').tooltip({
-    animated: 'fade',
-    placement: 'top',
+$('button[name="remove_levels"]').on('click', function(e){
+    var $form=$(this).closest('form');
+    e.preventDefault();
+    $('#confirm').modal({ backdrop: 'static', keyboard: false })
+        .one('click', '#delete', function (e) {
+            $form.trigger('submit');
+        });
 });
-</script>
-<script type="text/javascript">
+
 function hide(el_id){
   var el=document.getElementById(el_id);
   if(el_id.style.display!="none"){
@@ -381,7 +326,6 @@ function hide(el_id){
     el_id.style.display="";
   }
 }
-
 $(document).ready(function(){
     $("[rel=tooltip]").tooltip({ placement: 'top'});
 });
@@ -401,32 +345,5 @@ $(document).ready(function() {
 
 </script>
 
-</div></div>
-</div>
-
-</ul>
-<!-- Form Code Start -->
-
-<!-- client-side Form Validations:
-Uses the excellent form validation script from JavaScript-coder.com-->
-
-<!--
-Form Code End (see html-form-guide.com for more info.)
--->
-
-<div class="content-wrapper-profile content-wrapper">
-
-<form id="client_search" class="pure-form pure-form-aligned" action='/profile-home.php' method='post' accept-charset='UTF-8'>
-<fieldset >
-<input type='hidden' name='client_search' id='client_search' value='1'/>
-<input type='hidden'  class='spmhidip' name='spe8306af0d23b01a2bf7c1734aacf25fe' />
-<div><span class='error'></span></div>
-        <input type="text" class="content-leftalign content-fa-align" data-toggle="popover" data-trigger="hover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." name="clientID" id="clientID" clientID placeholder ='Busca por CPF' required></input>
-
-        <a class="blue btn" href="#"><i class="content-fa-align fa fa-arrow-circle-right fa-2x" type='submit' name='Submit' ></i></a>
-</fieldset>
-</form>
-</div>
-
-</body>
+</div></body>
 </html>
