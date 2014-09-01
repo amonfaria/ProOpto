21b4ce7c68de4cc25ba928ece30c603d39405ef7
amongoiano <amon.faria@gmail.com>
2014-09-01 01:52:22 -0400

    Changed files exist - Auto updating

--- test3.php
+++ test3.php
@@ -1,3 +1,8 @@
 <?php 
   echo $_POST['action'];
+  
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  $txt = $_POST['action'];
+  fwrite($myfile, $txt);
+  fclose($myfile);
  ?>
