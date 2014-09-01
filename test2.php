1ee8068253ad0478ec88f5b6f353bb3bf39fea2f
amongoiano <amon.faria@gmail.com>
2014-09-01 00:30:56 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -342,8 +342,7 @@ $(document).ready(function() {
 	$('a[data-confirm]').click(function(ev) {
 		var href = $(this).attr('href');
 		if (!$('#dataConfirmModal').length) {
-			$('body').append('<div id="dataConfirmModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header">
-			  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div><div class="modal-body">Certeza que quer excluir?</div><div class="modal-footer"><button class="button-small-green pure-button pure-button-primary" data-dismiss="modal" aria-hidden="true">Cancelar</button><button class="button-small-red pure-button pure-button-primary" id="dataConfirmOK">Excluir</button></div></div>');
+			$('body').append('<div id="dataConfirmModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"></div><div class="modal-body">Certeza que quer excluir?</div><div class="modal-footer"><button class="button-small-green pure-button pure-button-primary" data-dismiss="modal" aria-hidden="true">Cancelar</button><button class="button-small-red pure-button pure-button-primary" id="dataConfirmOK">Excluir</button></div></div>');
 		} 
 		$('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
 		$('#dataConfirmOK').attr('href', href);
