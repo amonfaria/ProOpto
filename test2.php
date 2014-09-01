4348a891a5bf4aaf782918bba7cb2626c5d830c8
amongoiano <amon.faria@gmail.com>
2014-09-01 02:29:00 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -1,23 +1,11 @@
 <?php
     include("main-header.php");
+    require_once('delete_confirm.php');
+    
 ?>
-<html>
-<script language="javascript">
-function fun(str)
-{
 
-    $.ajax ({
-        url: "test3.php",
-        data: { action : str }, //optional
-        success: function( result ) {
-            //do something after you receive the result
-        }
-
-}
-</script>
-<body>
-<button type="button" name="submit" value="test1" onClick="fun(this.value)">Test1</button>
-<button type="button" name="submit" value="test2" onClick="fun(this.value)">Test2</button>
-
-</body>
-</html>
+<form method="POST" action="test3.php" accept-charset="UTF-8">
+    <button class="btn btn-xs btn-danger" type="button" data-toggle="modal" name="amon" value="amon" data-target="#confirmDelete" data-title="Delete User" data-message="Are you sure you want to delete this user ?">
+        <i class="glyphicon glyphicon-trash"></i> Delete
+    </button>
+</form>
--- test3.php
+++ test3.php
@@ -1,8 +1,8 @@
 <?php 
-  echo $_POST['action'];
+  echo $_POST['name'];
   
   $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-  $txt = $_POST['action'];
+  $txt = $_POST['name'];
   fwrite($myfile, $txt);
   fclose($myfile);
  ?>
