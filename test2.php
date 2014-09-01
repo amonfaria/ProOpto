d51362bf758de3638fc76f2c125dfb3aa7d3b884
amongoiano <amon.faria@gmail.com>
2014-09-01 02:36:52 -0400

    Changed files exist - Auto updating

--- test2.php
+++ test2.php
@@ -6,7 +6,7 @@
 
 <form method="POST" action="test3.php" accept-charset="UTF-8">
 <input type='hidden' name='client_search' id='client_search' value='amon'/>
-    <button class="btn btn-xs btn-danger" type="button" data-toggle="modal" name="amon" value="amon" data-target="#confirmDelete" data-title="Delete User" data-message="Tem certeza que quer excluir usuario? ?">
+    <button class="btn btn-xs btn-danger" type="button" data-toggle="modal" name="amon" value="amon" data-target="#confirmDelete" data-title="Excluir Usuario" data-message="Tem certeza que quer excluir usuario? ?">
         <i class="glyphicon glyphicon-trash"></i> Excluir
     </button>
 </form>
